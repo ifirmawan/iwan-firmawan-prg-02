4838294743b480f42f078c7d3fb1c3d6dc3c4133
iwanf <firmawaneiwan@gmail.com>
2020-10-25 15:51:02 +0700

    WIP develop chart line

--- components/charts/line.js
+++ components/charts/line.js
@@ -1,7 +1,15 @@
-export const LineChart = () => {
+import { XYPlot, LineSeries } from 'react-vis'
+export const LineChart = ({ source }) => {
+  console.log(source)
   return (
     <>
-      Line
+      {source && (
+        <>
+          <XYPlot height={300} width={300}>
+            <LineSeries data={source} />
+          </XYPlot>
+        </>
+      )}
     </>
   )
 }
--- pages/dashboard/index.js
+++ pages/dashboard/index.js
@@ -1,13 +1,34 @@
-import { useState } from 'react'
+//import { useState } from 'react'
 import { LineChart, PieChart } from '../../components/charts'
-const DashboardPage = () => {
-  const [reports, setReports] = useState([])
+const DashboardPage = ({ pid }) => {
+  //const [reports, setReports] = useState([])
+  const exampleLine = [
+    { x: 0, y: 8 },
+    { x: 1, y: 5 },
+    { x: 2, y: 4 },
+    { x: 3, y: 9 },
+    { x: 4, y: 1 },
+    { x: 5, y: 7 },
+    { x: 6, y: 6 },
+    { x: 7, y: 3 },
+    { x: 8, y: 2 },
+    { x: 9, y: 0 }
+  ]
+  const reports = []
   return (
     <>
-      <h1>Dashboard</h1>
-      <LineChart />
+      <h1>Dashboard {pid}</h1>
+      <LineChart source={exampleLine} />
       <PieChart source={reports} />
     </>
   )
+}
+DashboardPage.getInitialProps = (req) => {
+  const { query } = req
+  const { id: pid } = query || {}
+  console.log(query)
+  return {
+    pid: pid
+  }
 }
 export default DashboardPage
